e41f488fb2f7b7a497d2b8d7d00fc11e1c6fd1f7
MarleneArvedahl <marlene.arvedahl@gmail.com>
2023-11-10 12:01:39 +0100

    added code
    första sidan i princip klar, testar mig fram i js

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+// const mercuruElem = document.querySelector('#mercuru');
+// const venusElem = document.querySelector('#venus');
+// const earthElem = document.querySelector('#earth');
+// const marsElem = document.querySelector('#mars');
+// const jupiterElem = document.querySelector('#jupiter');
+// const saturnElem = document.querySelector('#saturn');
+// const uraniusElem = document.querySelector('uranius');
+// const neptuneElem = document.querySelector('#neptune');
+
+// const API_KEY = 'solaris-1Cqgm3S6nlMechWO';
+// const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/';
+
+// function displayPlanets(planets) {
+//     let allPlanets = '';
+    
+//     for(let i = 0; i < planets.length; i++) {
+//         console.log(planets[i]);
+//         allPlanets = allPlanets + `
+//         `
+//     }
+// }
+
+// async function getPlanets() {
+//    try {
+//     let resp = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
+//         method: "GET",
+//         headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" }
+//     });
+//   const data = await response.json();
+//   console.log(data);
+
+//    } catch (error) {
+//     displayPlanets(data)
+//     console.log(error + 'could not give you any planets');
+//    }
+// }
+
+// getPlanets();
+
