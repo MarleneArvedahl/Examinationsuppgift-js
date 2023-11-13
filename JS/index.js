2891a1720bb68dd70c43eec2178c905e1004c608
MarleneArvedahl <marlene.arvedahl@gmail.com>
2023-11-13 20:51:39 +0100

    added code
    mer gjort

--- JS/index.js
+++ JS/index.js
@@ -1,39 +1,73 @@
+const sunElem = document.querySelector("#sun");
 // const mercuruElem = document.querySelector('#mercuru');
 // const venusElem = document.querySelector('#venus');
 // const earthElem = document.querySelector('#earth');
 // const marsElem = document.querySelector('#mars');
 // const jupiterElem = document.querySelector('#jupiter');
 // const saturnElem = document.querySelector('#saturn');
-// const uraniusElem = document.querySelector('uranius');
+// const uraniusElem = document.querySelector('#uranius');
 // const neptuneElem = document.querySelector('#neptune');
+const planetsElem = document.querySelector("#planets");
 
-// const API_KEY = 'solaris-1Cqgm3S6nlMechWO';
-// const BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/';
+const BASE_URL = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/";
+// const API_KEY = 'solaris-1Cqgm3S6nlMechWO'; // för godkänt nyckel
 
-// function displayPlanets(planets) {
-//     let allPlanets = '';
-    
-//     for(let i = 0; i < planets.length; i++) {
-//         console.log(planets[i]);
-//         allPlanets = allPlanets + `
-//         `
-//     }
-// }
+//hämtar nyckeln och lägger den i en variabel
+async function getKey() {
+  let response = await fetch(
+    "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys",
+    {
+      method: "POST",
+    }
+  );
+  const data = await response.json();
+  return data.key;
+}
 
-// async function getPlanets() {
-//    try {
-//     let resp = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
-//         method: "GET",
-//         headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" }
-//     });
-//   const data = await response.json();
-//   console.log(data);
+//hämtar alla planeter och solen
+async function getBodies() {
+  const myKey = await getKey();
+  console.log(myKey);
 
-//    } catch (error) {
-//     displayPlanets(data)
-//     console.log(error + 'could not give you any planets');
-//    }
-// }
+  let response = await fetch(
+    "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+    {
+      method: "GET",
+      headers: { "x-zocom": myKey },
+    }
+  );
+  const data = await response.json();
+  return data.bodies;
+}
 
-// getPlanets();
+function addInfo(bodyID, data) {
+  console.log(`Add info to ${bodyID}`, data);
+  document.querySelector("#name").innerHTML = data.name;
+}
+
+const bodyElementIdToIndex = {
+  sun: 0,
+  mercuru: 1,
+  venus: 2,
+  earth: 3,
+  mars: 4,
+  jupiter: 5,
+  saturn: 6,
+  uranius: 7,
+  neptune: 8,
+};
+
+//async loadData gör att den väntar på att planeterna kommit, nästa -ett klickevent.
+async function loadData() {
+  const bodies = await getBodies();
+
+  planetsElem.addEventListener("click", (event) => {
+    console.log("planeten klickar", event.target.id);
+    const index = bodyElementIdToIndex[event.target.id];
+    const planet = bodies[index];
+    addInfo(event.target.id, planet);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loadData);
 
